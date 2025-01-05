9486c59620cfdb248e28b5e7dd73de87740ffb24
KOSASIH <kosasihg88@gmail.com>
2025-01-05 12:51:47 +0700

    Create transaction.py

---
+++ src/blockchain/transaction.py
@@ -0,0 +1,15 @@
+class Transaction:
+    def __init__(self, sender, recipient, amount):
+        self.sender = sender
+        self.recipient = recipient
+        self.amount = amount
+
+    def to_dict(self):
+        return {
+            "sender": self.sender,
+            "recipient": self.recipient,
+            "amount": self.amount,
+        }
+
+    def __repr__(self):
+        return f"Transaction(sender='{self.sender}', recipient='{self.recipient}', amount={self.amount})"
